ac76bfd41ef518771c1304632eca780303cdd231
Michail Kurachkin <michail.kurachkin@promwad.com>
2015-12-15 11:21:10 +0300

    add win+shift+i for change wallpaper

--- wallpapers.lua
+++ wallpapers.lua
@@ -1,4 +1,3 @@
-
 wallpaper_dirs = {"'/home/stelhs/MyPhoto/Объекты/'",
                   "'/home/stelhs/MyPhoto/Походы/'",
                   "'/home/stelhs/MyPhoto/Поездки/'"}
@@ -9,13 +8,20 @@ for _,path in pairs(wallpaper_dirs) do
   os.execute("find " .. path .. " -type f -name '*.JPG' -print0 >> /tmp/wallpaper_list")
 end
 
-mytimer = timer { timeout = 0 }
-mytimer:add_signal("timeout", function()
+wallpapers_timer = timer { timeout = 0 }
+wallpapers_timer:add_signal("timeout", function()
   if whereis_app('feh') then
 	  os.execute("cat /tmp/wallpaper_list | shuf -n1 -z | xargs -0 feh --bg-scale")
   end
-  mytimer:stop()
-  mytimer.timeout = math.random( 600, 1200)
-  mytimer:start()
+  wallpapers_timer:stop()
+  wallpapers_timer.timeout = math.random( 600, 1200)
+  wallpapers_timer:start()
 end)
-mytimer:start()
+wallpapers_timer:start()
+
+function wallpapers_update()  
+  os.execute("cat /tmp/wallpaper_list | shuf -n1 -z | xargs -0 feh --bg-scale")
+  wallpapers_timer:stop()
+  wallpapers_timer.timeout = math.random( 600, 1200)
+  wallpapers_timer:start()
+end
