13dddcc6122facc9b836be0c07d6e30d366bc045
Michail Kurockin <Michail.Kurachkin@promwad.com>
2018-03-03 14:27:37 +0300

    modify display_switcher after migration on ubuntu 16.04

--- display_switcher.sh
+++ display_switcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xrandr --output LVDS1 --auto --output HDMI1 --auto
+xrandr --output LVDS-1-1 --auto --output HDMI-1-1 --auto
 sleep 1
-xrandr --output LVDS1 --primary --below HDMI1
+xrandr --output LVDS-1-1 --primary --below HDMI-1-1
